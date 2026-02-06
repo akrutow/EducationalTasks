2503d5720778ebe9e9be19383906c3f9a25bf920
Artem Krutov <aikrutow@gmail.com>
2026-02-06 18:33:55 +0300

    Another interesting task

--- 7.py
+++ 7.py
@@ -1,15 +1,46 @@
-class DevelopmentTeam:
-    junior = []
-    senior = []
+class LoopTracker:
+    def __init__(self, iterable):
+        self.iterable = tuple(iterable)
+        self.index = -1
+        self.stop_counter = 0
+        self.last_elem = None
+        self.generic_counter = 0
 
-    def add_junior(self, *args):
-        for i in args:
-            self.junior.append((i, 'junior'))
+    @property
+    def accesses(self):
+        return self.generic_counter
 
-    def add_senior(self, *args):
-        for i in args:
-            self.senior.append((i, 'senior'))
+    @property
+    def empty_accesses(self):
+        return self.stop_counter  
+
+    @property
+    def first(self):
+        if list(self.iterable):
+            return list(self.iterable)[0]
+        else:
+            raise AttributeError('Исходный итерируемый объект пуст')
+
+    @property
+    def last(self):
+        if self.last_elem:
+            return self.last_elem
+        else:
+            raise AttributeError('Последнего элемента нет')
+
+    def is_empty(self):
+        if self.index >= len(self.iterable)-1:
+            return True
+        return False
 
     def __iter__(self):
-        yield from self.junior
-        yield from self.senior
+        return self
+    
+    def __next__(self):
+        self.index += 1
+        if self.index >= len(self.iterable):
+            self.stop_counter += 1
+            raise StopIteration
+        self.generic_counter += 1
+        self.last_elem = self.iterable[self.index]
+        return self.last_elem
