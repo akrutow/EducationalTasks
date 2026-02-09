9a082fa5ec24064b7a43bcf9e9a5ad4c7e31f0bf
Artem Krutov <aikrutow@gmail.com>
2026-02-09 22:54:14 +0300

    Solving a new task

--- 8.py
+++ 8.py
@@ -1,21 +1,36 @@
-class CyclicList:
-    def __init__(self, iterable=None):
-        if iterable is None:
+class OrderedSet:
+    def __init__(self, iterable=None) -> None:
+        if iterable:
+            self.iterable = list(iterable)
+        else:
             self.iterable = []
-        else:
-            self.iterable = iterable.copy()
 
-    def append(self, obj):
-        return self.iterable.append(obj)
+    def add(self, item):
+        self.iterable.append(item)
+        return set(self.iterable)
 
-    def pop(self, i=None):
-        if i is None:
-            return self.iterable.pop(-1)
-        else:
-            return self.iterable.pop(i)
+    def discard(self, item):
+        if item in self.iterable:
+            self.iterable.remove(item)
+        return set(self.iterable)
 
     def __len__(self):
-        return len(self.iterable)
+        return len(set(self.iterable))
+
+    def __iter__(self):
+        result = []
+        for i in self.iterable:
+            if i not in result:
+                result.append(i)
+        yield from result
+
+    def __contains__(self, i):
+        return i in set(self.iterable)
     
-    def __getitem__(self, key):
-        return self.iterable[key % len(self.iterable)]
+    def __eq__(self, other):
+        if isinstance(other, set):
+            return set(self.iterable) == other
+        elif isinstance(other, OrderedSet):
+            return self.iterable == other.iterable
+        return NotImplemented
+
