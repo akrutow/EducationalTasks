1e90537f1cfb621ef5c9bd25796f380308620407
Artem Krutov <aikrutow@gmail.com>
2026-02-14 18:21:12 +0300

    Solving a new task

--- 10.py
+++ 10.py
@@ -1,32 +1,21 @@
-class Grouper:
-    def __init__(self, iterable, key):
-        self.result = {}
-        self.key = key
-        for i in iterable:
-            if self.key(i) not in self.result:
-                self.result[self.key(i)] = []
-                self.result[self.key(i)].append(i)
-            else:
-                self.result[self.key(i)].append(i)
-        
-    def add(self, item):
-        if self.key(item) not in self.result:
-                self.result[self.key(item)] = []
-                self.result[self.key(item)].append(item)
-        else:
-            self.result[self.key(item)].append(item)
+class BasicPlan:
+    can_stream = True
+    can_download = True
+    has_SD = True
+    has_HD = False
+    has_UHD = False
+    num_of_devices = 1
+    price = '8.99$'
 
-    def group_for(self, item):
-        return self.key(item)
 
-    def __len__(self):
-        return len(self.result)
-    
-    def __iter__(self):
-        yield from tuple(self.result.items())
+class SilverPlan(BasicPlan):
+    has_HD = True
+    num_of_devices = 2
+    price = '12.99$'
 
-    def __contains__(self, item):
-        return item in self.result
 
-    def __getitem__(self, key):
-        return self.result[key]
+class GoldPlan(BasicPlan):
+    has_HD = True
+    has_UHD = True
+    num_of_devices = 4
+    price = '15.99$'
