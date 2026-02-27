2ae810327ed57093f2bbc3803934334737acfc18
Artem Krutov <aikrutow@gmail.com>
2026-02-28 00:11:03 +0300

    hmm

--- 1.py
+++ 1.py
@@ -1,27 +1,14 @@
-class Publication:
-    def __init__(self, title, author, year):
-        self.title = title
-        self._author = author
-        self._year = year
+class GameObject:
+    __slots__ = ('x', 'y')
 
-    def get_info(self):
-        return f'"{self.title}" ({self._author}, {self._year})'
-    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-class Book(Publication):
-    def __init__(self, title, author, year, isbn):
-        super().__init__(title, author, year)
-        self.isbn = isbn
 
-    def get_info(self):
-        return f'"{self.title}" ({self._author}, {self._year}), ISBN: {self.isbn}'
-    
+class Player(GameObject):
+    __slots__ = ('nickname', )
 
-class Magazine(Publication):
-    def __init__(self, title, editor, year, issue_number):
-        super().__init__(title, editor, year)
-        self.issue_number = issue_number
-
-    def get_info(self):
-        return f'"{self.title}" (Ред. {self._author}, {self._year}), Выпуск №{self.issue_number}'
-    
+    def __init__(self, x, y, nickname):
+        super().__init__(x, y)
+        self.nickname = nickname
